4003aa84be4d11231497dc4f3cdb67da0dd0f73a
Shawn Carpenter <shawncarpenter.co@gmail.com>
2024-01-12 17:39:14 -0700

    Save credentials and add routing for first endpoint

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,10 @@
 Rails.application.routes.draw do
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
-
+  namespace :api do
+    namespace :v0 do
+      get "/forecast", to: "weather#forecast"
+    end
+  end
   # Defines the root path route ("/")
   # root "articles#index"
 end
