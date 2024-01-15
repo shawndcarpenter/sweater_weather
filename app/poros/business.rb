3881d58e21cdfa93acca0a3d55b683dc6bdb266b
Shawn Carpenter <shawncarpenter.co@gmail.com>
2024-01-14 18:19:27 -0700

    Fix format of address

--- app/poros/business.rb
+++ app/poros/business.rb
@@ -1,8 +1,9 @@
 class Business
   attr_reader :name, :address, :rating, :reviews
+  
   def initialize(data)
     @name = data[:name]
-    @address = data[:location][:display_address].first
+    @address = data[:location][:display_address].join(" ")
     @rating = data[:rating]
     @reviews = data[:review_count]
   end
--- app/poros/munchie.rb
+++ app/poros/munchie.rb
@@ -1,5 +1,6 @@
 class Munchie
   attr_reader :destination_city, :forecast, :restaurant, :id
+  
   def initialize(destination_data, business_data, weather_data)
     @id = nil
     @destination_city = destination_data
