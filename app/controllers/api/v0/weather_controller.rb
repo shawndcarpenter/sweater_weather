c449344ba85ce45267de08edd77841b14c2997ad
Shawn Carpenter <shawncarpenter.co@gmail.com>
2024-01-16 15:44:16 -0700

    Add error handling for location does not exist

--- app/controllers/api/v0/weather_controller.rb
+++ app/controllers/api/v0/weather_controller.rb
@@ -1,14 +1,17 @@
 class  Api::V0::WeatherController < ApplicationController
   def forecast
-    geo = get_geocoded_location(params[:location])
-    if geo.class == Geocode
-      render json: 
-        ForecastSerializer.new(get_forecast(geo.latitude, 
-                                            geo.longitude))
+    if !params[:location]
+      missing_location_response
     else
-      invalid_locations_response
+      geo = get_geocoded_location(params[:location])
+      if geo.class == Geocode
+        render json: 
+          ForecastSerializer.new(get_forecast(geo.latitude, 
+                                              geo.longitude))
+      else
+        invalid_locations_response
+      end
     end
-
   end
   
   private
@@ -23,6 +26,11 @@ class  Api::V0::WeatherController < ApplicationController
 
   def invalid_locations_response
     render json: ErrorSerializer.new(ErrorMessage.new("Validation failed: location must be a real place", 422))
+    .serialize_json, status: 422
+  end
+
+  def missing_location_response
+    render json: ErrorSerializer.new(ErrorMessage.new("Validation failed: location must be present", 422))
     .serialize_json, status: 422
   end
 end
